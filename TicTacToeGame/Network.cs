2d8f537da0c2bb58add7c9e07bb64e327548e18f
Roshan Alwis <alwisroshan.13@cse.mrt.ac.lk>
2015-06-29 11:06:15 +0530

    Create a Network Class

---
+++ TicTacToeGame/Network.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworksApi.TCP.SERVER;
+using NetworksApi.TCP.CLIENT;
+
+namespace TicTacToeGame
+{
+    class Network
+    {
+        Server server;
+        Client client;
+        private Network()
+        {
+
+        }
+
+        public void startServer()
+        {
+
+        }
+
+        public void connectToServer()
+        {
+
+        }
+    }
+}
